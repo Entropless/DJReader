fda323ccb001bdf75ec013320d13cbe5a0ca5bec
18911971839@163.com <18911971839@163.com>
2019-01-10 22:55:15 +0800

    2019.1.10 night

--- app/src/main/java/com/wy/djreader/services/download/DownloadService.java
+++ app/src/main/java/com/wy/djreader/services/download/DownloadService.java
@@ -2,10 +2,28 @@ package com.wy.djreader.services.download;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.Nullable;
 
 public class DownloadService extends Service {
+    private DownloadHandler downloadHandler = null;
+
+    private final class DownloadHandler extends Handler{
+        public DownloadHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            //下载文件
+
+        }
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -16,11 +34,19 @@ public class DownloadService extends Service {
     public void onCreate() {
         super.onCreate();
         //创建一个Handler线程
+        HandlerThread downloadThread = new HandlerThread("downloadThread");
+        downloadThread.start();
+        //获取Looper，传入Handler
+        Looper looper = downloadThread.getLooper();
+        downloadHandler = new DownloadHandler(looper);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
-        return super.onStartCommand(intent, flags, startId);
+        Message msg = downloadHandler.obtainMessage();
+        msg.arg1 = startId;
+        msg.obj = intent;
+        downloadHandler.sendMessage(msg);
+        return START_REDELIVER_INTENT;//重启服务时传递最后一个intent
     }
 }
