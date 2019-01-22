fe2ef87cdb583852fbcb66f1cde971949dceb4e6
Brainstorming-Idea <18911971839@163.com>
2019-01-22 18:19:18 +0800

    2019.1.22

--- app/src/main/java/com/wy/djreader/main/view/MainActivity.java
+++ app/src/main/java/com/wy/djreader/main/view/MainActivity.java
@@ -14,7 +14,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
