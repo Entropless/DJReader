d08e6a965f1ee9c23dfcdafaa244f8b6bbcabc7f
18911971839@163.com <18911971839@163.com>
2019-03-03 22:32:47 +0800

    2019.3.3

--- djsign_system/src/main/java/com/dianju_wy/showpdf/DJContentView.java
+++ djsign_system/src/main/java/com/dianju_wy/showpdf/DJContentView.java
@@ -2,6 +2,9 @@ package com.dianju_wy.showpdf;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
@@ -17,9 +20,20 @@ public class DJContentView extends SurfaceView implements SurfaceHolder.Callback
     private SurfaceHolder surfaceHolder;//surface的持有者
     private Canvas canvas;//绘图的画布
     private boolean mIsDrawing;//控制绘图线程的标志位
+    private Paint paint;
+    private Path path;
+    private float x,y;//点击的坐标
 
     public DJContentView(Context context) {
         super(context);
+        paint = new Paint();
+        paint.setColor(Color.BLACK);//设置画笔颜色
+        paint.setStyle(Paint.Style.STROKE);//划线
+        paint.setStrokeWidth(10);//设置线的宽度
+        paint.setAntiAlias(true);//设置抗锯齿
+        //设置初始坐标
+        path =  new Path();
+//        path.moveTo(0,100);
         initView();
     }
 
@@ -43,13 +57,17 @@ public class DJContentView extends SurfaceView implements SurfaceHolder.Callback
     private void initView(){
         surfaceHolder = getHolder();
         surfaceHolder.addCallback(this);
-        //配置出事参数
+        //配置绘制参数
         setFocusable(true);
+        setFocusableInTouchMode(true);
+        setKeepScreenOn(true);
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-
+        mIsDrawing = true;
+        //开启绘制线程
+        new Thread(this).start();
     }
 
     @Override
@@ -59,16 +77,45 @@ public class DJContentView extends SurfaceView implements SurfaceHolder.Callback
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
+        mIsDrawing = false;
 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        x = event.getX();
+        y = event.getY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(x,y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(x,y);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 
     @Override
     public void run() {
+        while (mIsDrawing){
+            draw();
+        }
+    }
+    private void draw(){
+        try {
+            canvas = surfaceHolder.lockCanvas();//获取canvas绘图工具
+            canvas.drawColor(Color.WHITE);//绘制画布颜色
+            canvas.drawPath(path,paint);
+        }catch (Exception e){
 
+        }finally {
+            //释放canvas对象并提交画布
+            surfaceHolder.unlockCanvasAndPost(canvas);
+        }
     }
 }
--- djsign_system/src/main/java/com/dianju_wy/showpdf/MainActivity.java
+++ djsign_system/src/main/java/com/dianju_wy/showpdf/MainActivity.java
@@ -2,14 +2,20 @@ package com.dianju_wy.showpdf;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LinearLayout contentLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contentLayout = findViewById(R.id.contentLayout);
+        DJContentView contentView = new DJContentView(this);
+        contentLayout.addView(contentView);
     }
 }
